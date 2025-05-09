3696d78e265bcf533bb728b91f359413a5a5f75f
Martinez-Manuel-Contact <contact.martinezmanuel@gmail.com>
2025-05-09 18:06:11 -0300

    Create 01_total_gastado_por_cliente.sql
    Agrego primer caso SQL - Total gastado por cliente

---
+++ sql-consolidation/01_total_gastado_por_cliente.sql
@@ -0,0 +1,12 @@
+-- OBJETIVO:
+-- Mostrar cuánto gastó cada cliente en total, cruzando órdenes, productos y detalles.
+
+SELECT
+	o.customerID AS ID_Cliente,
+	c.customerName AS Nombre_Cliente,
+	SUM(od.quantity * p.price) AS Total_Gastado
+FROM Orders o 
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+INNER JOIN OrderDetails od ON o.OrderID = od.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+GROUP BY o.customerID, c.customerName
